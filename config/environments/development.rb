73aeb9805a5611ace344362586c4fe4c9358a6c9
minoriakita <minori.akita@gmail.com>
2022-04-11 16:06:58 +0000

    [Update] 4-11 投稿機能作成4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.active_job.queue_adapter = :inline
+  
   config.hosts << "ecbe7269fafa4258aa8f0ff5573de4e7.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
